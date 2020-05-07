7cf7fa1a83f359f2d284e7a0ba496ca093166298
Adam Sarli <adam.sarli@gmail.com>
2020-05-07 17:49:25 -0400

    scale tone displays with voice

--- client/src/components/instruments/Voice.js
+++ client/src/components/instruments/Voice.js
@@ -2,38 +2,44 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const Voice = ({currentNote, currentKey, keyNote, scale, scaleIndex}) => {
-
-  // const convertNote = note => {
-  //   const name = note.split("/")[0]
-  //   const octave = note.split("/")[1]
-  //   return `${name.toUpperCase()}${octave}`
-  // }
-
   const note = scaleIndex === 0 ? keyNote[scale[scaleIndex]] : currentNote
   const name = scaleIndex === 0 ? note.split("/")[0].toUpperCase() : note.split(/\d/)[0].toUpperCase()
   const octave = scaleIndex === 0 ? parseInt(note.split("/")[1])-1 : note.split(/\D/)[1]
-  
+
+  const calculateScaleTone = () => {
+    if (scaleIndex === 0) return 1
+    return scale[(scaleIndex - 1)%scale.length] + 1
+  }
 
   return (
-    <div>
+    <div style={styles.wrapperStyle}>
       <div>
         <span style={styles.noteStyle}>{name}</span>
         <span style={styles.octaveStyle}>{octave}</span>
       </div>
-      {scaleIndex}
+      <div style={styles.scaleToneStyle}>
+        {calculateScaleTone()}
+      </div>
     </div>
   )
 }
 
 const styles = {
   wrapperStyle: {
-
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   noteStyle: {
     fontSize: "60px"
   },
   octaveStyle: {
     fontSize: "20px"
+  },
+  scaleToneStyle: {
+    fontSize: "40px",
+    fontWeight: 'bold'
   }
 }
 
