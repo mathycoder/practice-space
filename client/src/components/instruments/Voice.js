cc42b1e54b54b842ba7253894288177f263a9f2d
Adam Sarli <adam.sarli@gmail.com>
2020-05-07 18:14:58 -0400

    fixed voice display for flats and sharps

--- client/src/components/instruments/Voice.js
+++ client/src/components/instruments/Voice.js
@@ -2,9 +2,21 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const Voice = ({currentNote, currentKey, keyNote, scale, scaleIndex}) => {
-  const note = scaleIndex === 0 ? keyNote[scale[scaleIndex]] : currentNote
-  const name = scaleIndex === 0 ? note.split("/")[0].toUpperCase() : note.split(/\d/)[0].toUpperCase()
-  const octave = scaleIndex === 0 ? parseInt(note.split("/")[1])-1 : note.split(/\D/)[1]
+
+  // const octave = scaleIndex === 0 ? parseInt(note.split("/")[1])-1 : note.split(/\D/)[1]
+
+  const name = () => {
+    const note = scaleIndex === 0
+      ? keyNote[scale[0]].split("/")[0]
+      : keyNote[scale[(scaleIndex - 1)%scale.length]].split("/")[0]
+    return `${note.slice(0,1).toUpperCase()}${note.slice(1)}`
+  }
+
+  const octave = () => {
+    return scaleIndex === 0
+      ? parseInt(keyNote[scale[0]].slice(-1)) - 1
+      : parseInt(keyNote[scale[(scaleIndex - 1)%scale.length]].slice(-1)) - 1
+  }
 
   const calculateScaleTone = () => {
     if (scaleIndex === 0) return 1
@@ -14,8 +26,8 @@ const Voice = ({currentNote, currentKey, keyNote, scale, scaleIndex}) => {
   return (
     <div style={styles.wrapperStyle}>
       <div>
-        <span style={styles.noteStyle}>{name}</span>
-        <span style={styles.octaveStyle}>{octave}</span>
+        <span style={styles.noteStyle}>{name()}</span>
+        <span style={styles.octaveStyle}>{octave()}</span>
       </div>
       <div style={styles.scaleToneStyle}>
         {calculateScaleTone()}
