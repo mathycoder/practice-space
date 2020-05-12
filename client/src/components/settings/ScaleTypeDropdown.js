e5ed2fc8367566f9eb98473211a20c124fc7bb94
Adam Sarli <adam.sarli@gmail.com>
2020-05-12 13:05:19 -0400

    fixed error where major keys displayed for G#, D#, and A# keys

--- client/src/components/settings/ScaleTypeDropdown.js
+++ client/src/components/settings/ScaleTypeDropdown.js
@@ -4,8 +4,32 @@ import 'react-dropdown/style.css'
 import './dropdown.css'
 
 const ScaleTypeDropdown = ({ currentKey, scaleType, callback}) => {
-  const keysWithNatMin = ['Ab', 'A', 'A#', 'Bb', 'B', 'C', 'C#', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'G', 'G#']
-  const options = keysWithNatMin.includes(currentKey) ? ['major', 'nat. minor', 'harm. minor'] : ['major']
+  //const keysWithNatMin = ['Ab', 'A', 'A#', 'Bb', 'B', 'C', 'C#', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'G', 'G#']
+  //const majorKeys = ['Ab', 'A', 'Bb', 'B', 'Cb', 'C', 'C#', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'Gb', 'G']
+  //const options = keysWithNatMin.includes(currentKey) ? ['major', 'nat. minor', 'harm. minor'] : ['major']
+
+  const keyNotesObj = {
+    'Ab': ['major', 'nat. minor', 'harm. minor'],
+    'A': ['major', 'nat. minor', 'harm. minor'],
+    'A#': ['nat. minor', 'harm. minor'],
+    'Bb': ['major', 'nat. minor', 'harm. minor'],
+    'B': ['major', 'nat. minor', 'harm. minor'],
+    'Cb': ['major'],
+    'C': ['major', 'nat. minor', 'harm. minor'],
+    'C#': ['major', 'nat. minor', 'harm. minor'],
+    'Db': ['major'],
+    'D': ['major', 'nat. minor', 'harm. minor'],
+    'D#': ['nat. minor', 'harm. minor'],
+    'Eb': ['major', 'nat. minor', 'harm. minor'],
+    'E': ['major', 'nat. minor', 'harm. minor'],
+    'F': ['major', 'nat. minor', 'harm. minor'],
+    'F#': ['major', 'nat. minor', 'harm. minor'],
+    'Gb': ['major'],
+    'G': ['major', 'nat. minor', 'harm. minor'],
+    'G#': ['nat. minor', 'harm. minor'],
+  }
+
+  const options = keyNotesObj[currentKey]
 
   return (
     <div className="scale-dropdown" style={styles.dropdownWrapper}>
