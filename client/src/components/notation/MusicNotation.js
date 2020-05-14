76b3e534a5021a0d74c5047b5380b44961253950
Adam Sarli <adam.sarli@gmail.com>
2020-05-14 17:30:50 -0400

    added eighth notes when you switch to All 2x

--- client/src/components/notation/MusicNotation.js
+++ client/src/components/notation/MusicNotation.js
@@ -5,7 +5,7 @@ import useWindowDimensions from '../../hooks/useWindowDimensions.js'
 
 const MusicNotation = ({ currentNote, currentKey, scale, currentCategory,
                          keyNotes, scaleIndex, accidentals, scaleType,
-                         repeatTopNote, scaleShape }) => {
+                         repeatTopNote, scaleShape, scaleRepetition }) => {
   const [VF] = useState(Vex.Flow)
   const rendererRefs = useRef([])
   const contextRefs = useRef([])
@@ -55,7 +55,7 @@ const MusicNotation = ({ currentNote, currentKey, scale, currentCategory,
   }, [currentKey, scaleIndex, scaleType, scaleShape, scale, repeatTopNote])
 
 
-  const generateNotes = () => {
+  const generateNotes = (noteType) => {
     return scale.map((el, index) => {
       let currentNote = keyNotes[el]
       let accidental = null
@@ -74,8 +74,8 @@ const MusicNotation = ({ currentNote, currentKey, scale, currentCategory,
       }
 
       const myNote = !accidental
-        ? new VF.StaveNote({clef: "treble", keys: [currentNote], duration: '4'})
-        : new VF.StaveNote({clef: "treble", keys: [currentNote], duration: '4'}).
+        ? new VF.StaveNote({clef: "treble", keys: [currentNote], duration: `${noteType}`})
+        : new VF.StaveNote({clef: "treble", keys: [currentNote], duration: `${noteType}`}).
             addAccidental(0, new VF.Accidental(accidental))
 
       if (index === ((scaleIndex-1)%scale.length)) myNote.setStyle({fillStyle: "rgb(48, 140, 223)", strokeStyle: "rgb(48, 140, 223)"});
@@ -85,34 +85,55 @@ const MusicNotation = ({ currentNote, currentKey, scale, currentCategory,
   }
 
   const renderKey = () => {
+    const notesPerMeasure = scaleRepetition === 'All 2x' ? 8*2 : 8
+    const notesPerPage = notesPerMeasure*2
     const keySignature = `${currentKey}${scaleType.includes('minor') ? 'm' :''}`
     const currentNoteIndex = scaleIndex === 0 ? 0 : (scaleIndex-1) % (scale.length)
-    const pageNumber = Math.floor(currentNoteIndex / 16)
-    const totalPages = Math.ceil(scale.length / 16)
+    const pageNumber = Math.floor(currentNoteIndex / notesPerPage)
+    const totalPages = Math.ceil(scale.length / notesPerPage)
     const lastPage = pageNumber+1 === totalPages
-    const notesArray = generateNotes().slice(pageNumber*16, pageNumber*16 + 16)
+    const notesArray = generateNotes(notesPerMeasure/2).slice(pageNumber*notesPerPage, (pageNumber+1)*notesPerPage)
+    console.log(scaleRepetition)
+
 
     for (let i=0; i<=1; i++){
       // generate variables
       const context = contextRefs.current[i]
-      const notes = notesArray.slice(0 + 8*i, 8+8*i)
-      const notesWidth = (canvasWidth - accidentalWidth - trebleWidth)*(notes.length/8)
-      const staveWidth = notes.length === 8
+      const notes = notesArray.slice(0 + notesPerMeasure*i, notesPerMeasure*(1+i))
+      const notesWidth = (canvasWidth - accidentalWidth - trebleWidth)*(notes.length/notesPerMeasure)
+      const staveWidth = notes.length === notesPerMeasure
         ? canvasWidth - 5
         : notesWidth + accidentalWidth + trebleWidth
 
       if (notes.length > 0){
-        const lastMeasure = Math.floor((notesArray.length-1) / 8) === i && lastPage ? true : false
+        const lastMeasure = Math.floor((notesArray.length-1) / notesPerMeasure) === i && lastPage ? true : false
         const stave = new VF.Stave(0, 0, staveWidth)
         stave.addClef("treble").addKeySignature(keySignature)
         if (lastMeasure) stave.setEndBarType(VF.Barline.type.REPEAT_END)
 
         // build and draw the notes
-        const voice = new VF.Voice({num_beats: notes.length,  beat_value: 4});
+        const voice = new VF.Voice({num_beats: notes.length,  beat_value: notesPerMeasure/2});
         voice.addTickables(notes);
+        const beams = VF.Beam.generateBeams(notes,
+          {
+            stem_direction: 1,
+            groups: [new VF.Fraction(4, 8)]
+          })
+        {
         const formatter = new VF.Formatter().joinVoices([voice]).format([voice], notesWidth);
         stave.setContext(context).draw()
         voice.draw(context, stave)
+
+  // groups: [new Vex.Flow.Fraction(2, 8)],
+  // stem_direction: -1,
+  // beam_rests: true,
+  // beam_middle_only: true,
+  // show_stemlets: false
+}
+
+        beams.forEach(function(beam) {
+          beam.setContext(context).draw();
+        })
       }
     }
   }
@@ -156,6 +177,7 @@ const mapStateToProps = state => {
     accidentals: state.settings.accidentals,
     scaleType: state.settings.scaleType,
     scaleShape: state.settings.scaleShape,
+    scaleRepetition: state.settings.scaleRepetition,
     repeatTopNote: state.settings.repeatTopNote
   }
 }
