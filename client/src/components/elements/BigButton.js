a3d8862f62dd8baa5d2bc3f27bf5fbd05bf50c18
Adam Sarli <adam.sarli@gmail.com>
2020-05-18 19:30:06 -0400

    used <button /> to simplify styling of Start button

--- client/src/components/elements/BigButton.js
+++ client/src/components/elements/BigButton.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import { AwesomeButton } from "react-awesome-button";
-import "react-awesome-button/dist/styles.css";
+import React, { useRef } from 'react'
+// import { AwesomeButton } from "react-awesome-button";
+// import "react-awesome-button/dist/styles.css";
+import './css/button.css'
 
 const BigButton = ({ title, callback, disabled }) => {
+  const buttonRef = useRef(null)
   return (
-    <AwesomeButton
-      onPress={callback}
+    <button
+      ref={buttonRef}
+      className="big-button"
+      onClick={(e) => {
+        callback(e)
+        buttonRef.current.blur()
+      }}
       disabled={disabled}
-      type="primary">{title}</AwesomeButton>
+      >{title}</button>
   )
 }
 
 export default BigButton
+
+// return (
+//   <AwesomeButton
+//     className="awesome-button-custom"
+//     onPress={callback}
+//     disabled={disabled}
+//     type="primary">{title}</AwesomeButton>
+// )
